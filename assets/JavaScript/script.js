c1ad37fe9c766a088acf806c449f89a741df35da
Elvin <elvinsrkrov@gmail.com>
2024-02-13 21:36:24 +0400

    responsive design has created, workout form and form submit functionality added

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -22,41 +22,64 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
-    function (poistion) {
-      const { latitude, longitude } = poistion.coords;
+    function (position) {
+      const { latitude, longitude } = position.coords;
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 15);
+      map = L.map("map").setView(coords, 15);
       console.log(map);
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        const clickedCoords = [lat, lng];
-
-        L.marker(clickedCoords)
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: `cycling-popup`,
-            })
-          )
-          .setPopupContent("Salam Dostlar")
-          .openPopup();
+      //handling clicks on the map
+      map.on("click", function (e) {
+        mapEvent = e;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
     function () {
-      alert("Could not get your poistion!");
+      alert("Error!");
     }
   );
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+
+  //display the marker
+  const { lat, lng } = mapEvent.latlng;
+  const clickedCoords = [lat, lng];
+
+  L.marker(clickedCoords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: `cycling-popup`,
+      })
+    )
+    .setPopupContent("Salam Dostlar")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
